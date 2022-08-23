aa4a0245f53a9c15ce5ced981468c6303664a324
Dmitry Borisov <nagasadoy@gmail.com>
2022-08-23 23:44:18 +0300

    commit

--- src/tag/entities/tag.entity.ts
+++ src/tag/entities/tag.entity.ts
@@ -1,6 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { User } from "src/user/entities/user.entity";
 import { Column, Entity, JoinColumn, ManyToMany, ManyToOne, OneToMany, PrimaryColumn, PrimaryGeneratedColumn } from "typeorm";
+import { GetTagDto } from "../dto/get-tag.dto";
 
 @Entity('tag')
 export class Tag {
@@ -22,4 +23,9 @@ export class Tag {
     @ManyToOne(type => User)
     @JoinColumn({ name: 'creator' })
     user: User;
+
+    public convertToGetDto(tag){
+        let dto = new GetTagDto();
+        
+    }
 }
